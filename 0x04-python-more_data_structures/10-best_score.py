3b9fa0dc0fb2b84e683adc019849cae4de5aa033
SihamEl23 <elghazi.siham1101@gmail.com>
2023-08-16 02:20:51 -0700

    add tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best_key = None
-    best_value = -1
-    if a_dictionary == {}:
-        return None
-    for key, value in a_dictionary.items():
-        if value > best_value:
-            best_value = value
-            best_key = key
-    return best_key
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
