6ac6fb929ec11dc4c84e5f6ca1596ecf3a17750e
SihamEl23 <elghazi.siham1101@gmail.com>
2023-09-12 13:05:40 +0000

    done

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+4-inherits_from
+Check if the object is an instance from a class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if the object is an instance of a class inherited from a class
+    Returns True ptherwise False
+    """
+
+    return issubclass(type(obj), a_class) and type(obj) != a_class
