8431dae7650fde775e3c1eb45ecb524e18428833
SihamEl23 <elghazi.siham1101@gmail.com>
2023-10-19 11:08:22 -0700

    Finish the mandatory and advanced tasks

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+--  lists cities id, name and states name in the database hbtn_0d_usa.
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY;
